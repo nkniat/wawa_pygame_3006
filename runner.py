0589b3f6ebfbede531d993eaaeecd8cc6b3785c4
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-30 11:59:26 +0200

    prostokty

--- runner.py
+++ runner.py
@@ -9,7 +9,8 @@ clock = pygame.time.Clock()
 
 background_surface = pygame.image.load('images_PG/background.png').convert()
 mushroom_surface = pygame.image.load('images_PG/mashroom.png').convert_alpha()
-mushroom_pos_x = 500
+mushroom_rect = mushroom_surface.get_rect(topleft=(500, 300))
+# mushroom_pos_x = 500
 
 while True:
     for event in pygame.event.get():
@@ -20,10 +21,14 @@ while True:
     screen.blit(background_surface, (0, 0))
 
     step = 5
-    mushroom_pos_x -= step
-    if mushroom_pos_x < -100:
-        mushroom_pos_x = 700
-    screen.blit(mushroom_surface, (mushroom_pos_x, 300))
+    # mushroom_pos_x -= step
+    # if mushroom_pos_x < -100:
+    #     mushroom_pos_x = 700
+    mushroom_rect.x -= step
+    if mushroom_rect.right < 0:
+        mushroom_rect.left = 700
+    #screen.blit(mushroom_surface, (mushroom_pos_x, 300))
+    screen.blit(mushroom_surface, mushroom_rect)
 
     pygame.display.update()
     clock.tick(60)  # framerate (60fps)
