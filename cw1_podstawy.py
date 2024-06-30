b011511623b81817c75a26089c1dd875a4bac243
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-30 11:21:02 +0200

    obsluga klawiszy

--- cw1_podstawy.py
+++ cw1_podstawy.py
@@ -2,6 +2,7 @@ import pygame
 from sys import exit
 pygame.init()  # zainicjalizowanie silnika pygame
 
+X, Y = 400, 200
 WIDTH, HEIGHT = 800, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Moja pierwsza gra")
@@ -18,14 +19,29 @@ while True:
 
     # screen.blit(test_surface, (0, 0))
 
-    # pygame.draw.rect(screen, "White", (0, 50, 100, 100))
+
+    pygame.draw.rect(screen, "White", (X, Y, 10, 10))
     # pygame.draw.line(screen, (128, 128, 128), (10, 200), (10, 300), 5)
 
     # wypisywanie tekstu
-    font = pygame.font.SysFont("Arial", 24)
-    label = font.render("Hello World", 1, 'White')
-    screen.blit(label, (10, 10))
+    # font = pygame.font.SysFont("Arial", 24)
+    # label = font.render("Hello World", 1, 'White')
+    # screen.blit(label, (10, 10))
 
+    step = 5
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        X -= step
+    if keys[pygame.K_RIGHT]:
+        X += step
+    if keys[pygame.K_UP]:
+        Y -= step
+    if keys[pygame.K_DOWN]:
+        Y += step
+
+    screen.fill("Black")
+    pygame.draw.rect(screen, "White", (X, Y, 10, 10))
 
     pygame.display.update()
     clock.tick(60)  #framerate (60fps)
